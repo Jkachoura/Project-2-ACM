b667287d367a69f722c6eb7d54324e5b198a85ca
Jaouad Kachoura <91124093+Jkachoura@users.noreply.github.com>
2021-12-16 16:23:12 +0100

    Update test auto 4 wielen.ino

--- Testcodes/Motor/test auto 4 wielen.ino
+++ Testcodes/Motor/test auto 4 wielen.ino
@@ -1,15 +1,9 @@
-// Motor Front Left connections
+// Left motor connections
 int in1 = 1;
 int in2 = 2;
-// Motor Front Right connections
+// Right motors connections
 int in3 = 3;
 int in4 = 4;
-// Motor Back Left connections
-int in5 = 5;
-int in6 = 6;
-// Motor Back Right connections
-int in7 = 7;
-int in8 = 8;
 
 void setup() {
   // Set all the motor control pins to outputs
@@ -17,20 +11,12 @@ void setup() {
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
-  pinMode(in5, OUTPUT);
-  pinMode(in6, OUTPUT);
-  pinMode(in7, OUTPUT);
-  pinMode(in8, OUTPUT);
   
   // Turn off motors - Initial state
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);
-  digitalWrite(in5, LOW);
-  digitalWrite(in6, LOW);
-  digitalWrite(in7, LOW);
-  digitalWrite(in8, LOW);
 }
 
 void loop() {
@@ -47,10 +33,6 @@ void forward(){
   digitalWrite(in2, LOW);
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
-  digitalWrite(in5, HIGH);
-  digitalWrite(in6, LOW);
-  digitalWrite(in7, HIGH);
-  digitalWrite(in8, LOW);
 }
 
 void backward(){
@@ -58,32 +40,20 @@ void backward(){
   digitalWrite(in2, HIGH);
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
-  digitalWrite(in5, LOW);
-  digitalWrite(in6, HIGH);
-  digitalWrite(in7, LOW);
-  digitalWrite(in8, HIGH);
 }
 
 void turnLeft(){
   digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
-  digitalWrite(in5, LOW);
-  digitalWrite(in6, LOW);
-  digitalWrite(in7, LOW);
-  digitalWrite(in8, LOW);
 }
 
 void turnRight(){
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
-  digitalWrite(in5, LOW);
-  digitalWrite(in6, LOW);
-  digitalWrite(in7, LOW);
-  digitalWrite(in8, LOW);
 }
 
 void stopping(){
@@ -91,8 +61,4 @@ void stopping(){
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);
-  digitalWrite(in5, LOW);
-  digitalWrite(in6, LOW);
-  digitalWrite(in7, LOW);
-  digitalWrite(in8, LOW);
 }
