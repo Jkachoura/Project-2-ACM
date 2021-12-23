7ca7e4e30461111beb7d4f1d9aa9eac7f210e32d
SabriDemir123 <95883706+SabriDemir123@users.noreply.github.com>
2021-12-23 13:05:17 +0100

    Update Parkeren.ino

--- Testcodes/Testplan/Parkeren.ino
+++ Testcodes/Testplan/Parkeren.ino
@@ -1,4 +1,5 @@
 #include <WiFi.h>
+
 // Left Motor connections
 #define lM1 12
 #define lM2 13
@@ -6,102 +7,106 @@
 #define rM1 2
 #define rM2 15
 
-const char * ssid = "TMNL-063581";
-const char * password = "Kachoura010!";
+const char* ssid     = "Tesla IoT";
+const char* password = "fsL6HgjN";
 
 WiFiServer server(80);
 
-void setup() {
-  Serial.begin(115200);
+void setup()
+{
+    Serial.begin(115200);
+    pinMode(5, OUTPUT);      // set the LED pin mode
 
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+    delay(10);
 
-  WiFi.begin(ssid, password);
+    // We start by connecting to a WiFi network
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
 
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+    WiFi.begin(ssid, password);
 
-  server.begin();
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    
+    server.begin();
+
+    drive('p');
 }
+int value = 0;
 
-void loop() {
-    WiFiClient client = server.available(); // listen for incoming clients
+void loop(){
+ WiFiClient client = server.available();   // listen for incoming clients
 
-    if (client) { // if you get a client,
-      Serial.println("New Client."); // print a message out the serial port
-      String currentLine = ""; // make a String to hold incoming data from the client
-      while (client.connected()) { // loop while the client's connected
-        if (client.available()) { // if there's bytes to read from the client,
-          char c = client.read(); // read a byte, then
-          Serial.write(c); // print it out the serial monitor
-          if (c == '\n') { // if the byte is a newline character
+  if (client) {                             // if you get a client,
+    Serial.println("New Client.");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
 
-            // if the current line is blank, you got two newline characters in a row.
-            // that's the end of the client HTTP request, so send a response:
-            if (currentLine.length() == 0) {
-              // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-              // and a content-type so the client knows what's coming, then a blank line:
-              client.println("HTTP/1.1 200 OK");
-              client.println("Content-type:text/html");
-              client.println();
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
 
-              // the content of the HTTP response follows the header:
-              client.print("Click <a href=\"/F\">here</a> to drive forward.<br>");
-              client.print("Click <a href=\"/B\">here</a> to drive backward.<br>");
-              client.print("Click <a href=\"/L\">here</a> to turn left.<br>");
-              client.print("Click <a href=\"/R\">here</a> to turn right.<br>");
-              client.print("Click <a href=\"/S\">here</a> to turn stop.<br>");
-              client.print("Click <a href=\"/A\">here</a> to turn atonomous.<br>");
+            // the content of the HTTP response follows the header:
+            client.print("Click <a href=\"/F\">here</a> to go forward.<br>");
+            client.print("Click <a href=\"/B\">here</a> to go backward.<br>");
+            client.print("Click <a href=\"/L\">here</a> to turn left.<br>");
+            client.print("Click <a href=\"/R\">here</a> to turn right.<br>");
+            client.print("Click <a href=\"/S\">here</a> to stop.<br>");
 
-              // The HTTP response ends with another blank line:
-              client.println();
-              // break out of the while loop:
-              break;
-            } else { // if you got a newline, then clear currentLine:
-              currentLine = "";
-            }
-          } else if (c != '\r') { // if you got anything else but a carriage return character,
-            currentLine += c; // add it to the end of the currentLine
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {    // if you got a newline, then clear currentLine:
+            currentLine = "";
           }
-          // Check to see if the client request was "GET /F", "GET /B", "GET /L", "GET /R", "GET /S" or "GET /A"
-          if (currentLine.endsWith("GET /F")) {
-            drive('f');
-          }
-          if (currentLine.endsWith("GET /B")) {
-            drive('b');
-          }
-          if (currentLine.endsWith("GET /L")) {
-            drive('l');
-            delay(2500);
-            drive('f');
-          }
-          if (currentLine.endsWith("GET /R")) {
-            drive('r');
-            delay(2500);
-            drive('f');
-          }
-          if (currentLine.endsWith("GET /S")) {
-            drive('p');
-          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /F")) {
+          drive('f');
+        }
+        if (currentLine.endsWith("GET /B")) {
+          drive('b');
+        }
+        if (currentLine.endsWith("GET /L")) {
+          drive('l');
+        }
+        if (currentLine.endsWith("GET /R")) {
+          drive('r');
+        }
+        if (currentLine.endsWith("GET /S")) {
+          drive('p');
         }
       }
-      // close the connection:
-      client.stop();
-      Serial.println("Client Disconnected.");
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("Client Disconnected.");
   }
 }
+//Function to drive the motors to given direction
 void drive(char mode) {
   switch (mode) {
   case 'f':
