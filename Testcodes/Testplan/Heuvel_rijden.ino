a78168f2904e2bd863ffe7d79d88416116e585d8
Jaouad Kachoura <91124093+Jkachoura@users.noreply.github.com>
2022-01-27 13:58:21 +0100

    Update Heuvel_rijden.ino

--- Testcodes/Testplan/Heuvel_rijden.ino
+++ Testcodes/Testplan/Heuvel_rijden.ino
@@ -10,6 +10,12 @@
 //Ultrasonic Sensor
 #define echoPin 17
 #define trigPin 16
+//States of IR sensor
+int irLeftState;
+int irRightState;
+//Variable to see which IR last scanned the line
+boolean acmIsLeft = false;
+boolean acmIsRight = false;
 
 int distance;     // variable for the distance measurement
 long duration;    // variable for the traveltime of sound waves
@@ -30,6 +36,7 @@ void setup() {
 
 void loop() {
   drive('f');
+  checkIRs();
   checkSensor();
 }
 
@@ -68,6 +75,26 @@ void drive(char mode){
       break;
   }
 }
+void checkIRs(){
+  
+  //Putting the value what IR sensor gives into the state variable of the ir sensors
+  irLeftState = digitalRead(irLeft);
+  irRightState = digitalRead(irRight);
+  if(irLeftState == 1){
+    drive('r');
+    delay(200);
+    acmIsRight = false;
+    acmIsLeft = true;
+  }
+  if(irRightState == 1){
+    drive('l');
+    delay(200);
+    acmIsRight = true;
+    acmIsLeft = false;
+  }
+ 
+
+}
 
 int frontUltrasone(){
   // Clears the trigPin condition
@@ -86,9 +113,18 @@ int frontUltrasone(){
 }
 
 void checkSensor(){
-    if(frontUltrasone() <= 15){
-    drive('l');
-    delay(1000);
-    drive('f');
+  if(frontUltrasone() <= 15){
+    if(acmIsRight){
+      drive('b');
+      delay(1000);
+      drive('r');
+      delay(333);
+    }
+    else{
+      drive('b');
+      delay(1000);
+      drive('l');
+      delay(333);
+    }
   }
 }
